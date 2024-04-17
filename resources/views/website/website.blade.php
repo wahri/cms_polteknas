78c2de2cd999912005ea69bb165e978762ecad93
WnB <wahyubahri1212@gmail.com>
2024-04-17 09:39:33 +0700

    fix css

--- resources/views/website/website.blade.php
+++ resources/views/website/website.blade.php
@@ -39,6 +39,7 @@
         href='https://fonts.googleapis.com/css?family=Poppins%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CABeeZee%3Aregular%2Citalic&amp;subset=latin%2Clatin-ext%2Cdevanagari&amp;ver=5.0.3'
         type='text/css' media='all' />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
     @yield('styles')
 </head>
 
